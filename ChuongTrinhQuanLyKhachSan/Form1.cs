0638da5e8817e22c8a47089f4286154e72e43ec5
Lixus <lixus.studies@gmail.com>
2025-10-20 18:59:23 +0700

    first Commit

---
+++ ChuongTrinhQuanLyKhachSan/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChuongTrinhQuanLyKhachSan
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "Athinhtamgui" && txtPassword.Text == "123")
+            {
+                labelError.Visible = false;
+                Dashboard ds = new Dashboard();
+                this.Hide();
+                ds.Show();
+            }
+            else
+            {
+                labelError.Visible = true;
+                txtPassword.Clear();
+            }
+        }
+
+        private void guna2Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ChuongTrinhQuanLyKhachSan/fuction.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Windows.Forms;
+
+namespace ChuongTrinhQuanLyKhachSan
+{
+    internal class fuction
+    {
+        protected SqlConnection getConnection()
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = @"Data Source=DESKTOP-4KJH1F5\SQLEXPRESS;Initial Catalog=QuanLyKhachSan;Integrated Security=True";
+            return con;
+        }
+
+        public DataSet getData(String query)
+        {
+            SqlConnection con = getConnection();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = query;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            return ds;
+
+        }
+
+        public void setData(String query, String message)
+        {
+            SqlConnection con = getConnection();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            con.Open();
+            cmd.CommandText = query;
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            MessageBox.Show(message, "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        public SqlDataReader getForCombo(String query)
+        {
+            SqlConnection con = getConnection();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            con.Open();
+            cmd.CommandText = query;
+            SqlDataReader sdr = cmd.ExecuteReader();
+            return sdr;
+        }
+
+    }
+}
